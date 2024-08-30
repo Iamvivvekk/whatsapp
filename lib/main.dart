3d9938119e71eea38769393e69efd075f5a4abc2
Iamvivvekk <sarojvivek895@gmail.com>
2024-08-30 23:35:23 +0530

    responsive user interface is completed however the backend will start further from here & alse made some changes in gitIgnore

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp/responsive.dart';
 import 'package:whatsapp/theme/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
